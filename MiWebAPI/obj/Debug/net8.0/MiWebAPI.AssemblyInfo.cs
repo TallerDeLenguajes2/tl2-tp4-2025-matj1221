ac0a304dfab787bc0815060084c87fe34a21c538
[matj1221] <matiasjcalderon@hotmail.com>
2025-10-04 15:30:03 -0300

    tp5 finalizado.

--- MiWebAPI/obj/Debug/net8.0/MiWebAPI.AssemblyInfo.cs
+++ MiWebAPI/obj/Debug/net8.0/MiWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a68ea9df783ee124cbc27589c9865c09eb2a37f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19e1ec4d5494f9fc8c3d10130083a06a5419217d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
